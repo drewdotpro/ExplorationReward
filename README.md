f00ed905d8d60d20a321a88a5b20bab0d0bc9a56
tubakyle <tubakyle@gmail.com>
2014-08-08 22:58:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ExplorationReward
+This plugin runs a set of console commands when players discover new chunks.
+
+## Config
+The config will be generated after the first run with the plugin in the plugins folder.
+<br>Configuration instructions can be found within the config file.
+<br>The config can be used to configure what worlds the plugin operates in, and what commands are run when a player finds a new chunk.
+<br>Here is the default config:
+<pre>
+# This config contains commands to run when a player discovers a new chunk.
+# The commands will be run as console.
+# '%p' will be replaced with the player's name, '%w' will be replaced with the player's current world's name.
+# All commands support chatcolors.
+# Any number of commands and worlds may be added.
+worlds-to-operate-in:
+  - 'world'
+  - 'world_nether'
+  - 'world_the_end'
+commands:
+  - 'give %p diamond 1'
+  - 'say &r&6%p &rdiscovered a new chunk in &c ''%w''&r!'
+  - 'tell %p &rNice job! You discovered a &6new chunk&r&c!&r Take your reward.'
+</pre>
+
+## Commands
+This plugin does not contain any commands.
+
+## Permissions
+This plugin does not use any permissions.
---
+++ src/main/java/io/github/tubakyle/explorationreward/ExplorationReward.java
@@ -0,0 +1,44 @@
+package io.github.tubakyle.explorationreward;
+
+import io.github.tubakyle.kpslib.PlayerFinder;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkPopulateEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+/**
+ * Created by Kyle Sferrazza on 8/8/2014.
+ * This file is a part of: ExplorationReward.
+ * All rights reserved.
+ */
+public class ExplorationReward extends JavaPlugin implements Listener{
+    @Override
+    public void onEnable () {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onDiscover(ChunkPopulateEvent chunkPopulateEvent) {
+        Player player = PlayerFinder.findNearest(chunkPopulateEvent.getChunk());
+        reloadConfig();
+        List<String> enabledWorlds = getConfig().getStringList("worlds-to-operate-in");
+        if (!(enabledWorlds.contains(player.getWorld().getName()))) {
+            return;
+        }
+        List<String> commandsToRun = getConfig().getStringList("commands");
+        for (String command : commandsToRun) {
+            command = command.replace("%p", player.getDisplayName());
+            command = command.replace("%w", player.getWorld().getName());
+            command = ChatColor.translateAlternateColorCodes('&', command);
+            CommandSender console = getServer().getConsoleSender();
+            Bukkit.dispatchCommand(console, command);
+        }
+    }
+}
